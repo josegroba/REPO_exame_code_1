9c5c87a84edbd969184993bad6edeb6126e257da
Julio Mosquera <julio.mosquera@yahoo.es>
2022-05-01 20:32:24 +0200

    Feature. Persona

--- src/com/acarballeira/Persona.java
+++ src/com/acarballeira/Persona.java
@@ -2,4 +2,16 @@ package com.acarballeira;
 
 public class Persona {
 
+	private int edad;
+	private String nome;
+	
+	Persona(){}
+
+	public Persona(int edad, String nome) {
+		super();
+		this.edad = edad;
+		this.nome = nome;
+	}
+	
+	
 }
