d19d580e850749aa71481cd560158db078a6e8d8
Julio Mosquera <julio.mosquera@yahoo.es>
2022-05-01 20:34:19 +0200

    Feature. Coche

---
+++ src/com/acarballeira/Coche.java
@@ -0,0 +1,11 @@
+package com.acarballeira;
+
+public class Coche {
+	
+	private String matricula;
+	private String marca;
+	private String modelo;
+	private String version;
+	private String ano;
+	
+}
